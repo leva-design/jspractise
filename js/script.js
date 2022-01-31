de44d7bb4ba29a2b06ac0ee65e19efd166819393
Leva <example@gmail.com>
2022-01-31 20:42:12 +0200

    proggram first step

--- js/script.js
+++ js/script.js
@@ -5,28 +5,22 @@
 // Вся информация которая приходит от пользователя -- будет в виде строк!!!
 // Вся информация которая приходит от пользователя -- будет в виде строк!!!
 
-// console.log('3' + ' -object'); 
-// console.log(4 +'3')
+const numberOfFilms =  +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотреных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
 
 
-// let incr = 10,
-//     decr = 10;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// // ++incr;
-// // --decr;
-
-// console.log(++incr);
-// console.log(--decr);
-
-// console.log(5%2);
-
-// console.log(2*4 === 8);
-
-
-// const isChecked = true;
-// const isClose = true;
-// const you = true;
-
-// console.log(isChecked || !isClose);
-
-// console.log(2 + 2 * 2 !== '6');
+console.log(personalMovieDB);
