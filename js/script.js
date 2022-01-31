72701850dd09f9530854678c7c0dd0d47634d2ab
Leva <example@gmail.com>
2022-01-31 11:00:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+// Вся информация которая приходит от пользователя -- будет в виде строк!!!
+// Вся информация которая приходит от пользователя -- будет в виде строк!!!
+// Вся информация которая приходит от пользователя -- будет в виде строк!!!
+
+// console.log('3' + ' -object'); 
+// console.log(4 +'3')
+
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+
+// const isChecked = true;
+// const isClose = true;
+// const you = true;
+
+// console.log(isChecked || !isClose);
+
+// console.log(2 + 2 * 2 !== '6');
